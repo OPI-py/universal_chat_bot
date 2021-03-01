3cc0bacfaf2a203719549a54483a4b8355727212
OPI-py <oleksandrp09@gmail.com>
2021-03-01 22:29:08 +0200

    NLTK requires to download after 1st instalationl

--- training.py
+++ training.py
@@ -4,10 +4,11 @@ import pickle
 import numpy as np
 import nltk
 from nltk.stem import WordNetLemmatizer
-
 from tensorflow.keras.models import Sequential
 from tensorflow.keras.layers import Dense, Activation, Dropout
 from tensorflow.keras.optimizers import SGD
+
+# nltk.download('punkt') # require to download once
 
 lemmatizer = WordNetLemmatizer()
 
